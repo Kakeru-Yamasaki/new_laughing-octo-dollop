2b2ee9ee95b9bcbcb36f4db2b1d9bc76c581ee3e
Kakeru-Yamasaki <yamanomae144@gmail.com>
2023-06-13 22:04:21 +0900

    投稿の削除機能の追加

--- routes/web.php
+++ routes/web.php
@@ -18,4 +18,5 @@ Route::get('/', [PostController::class, 'index']);
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 Route::post('/posts', [PostController::class, 'store']);
+Route::delete('/posts/{post}', [PostController::class, 'delete']);
 //  '/posts{対象のIDデータ}'にGetリクエストが来たら、PostControllerのshowメソッドを実行する。
